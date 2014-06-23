9ed8993349e6e2a03aeef6c7ff1d07789a71da77
Beatlegger <beatlegger13@gmail.com>
2014-06-24 01:19:52 +0400

    Append test methods

--- Transmission.API.RPC.Test/MethodsTest.cs
+++ Transmission.API.RPC.Test/MethodsTest.cs
@@ -9,13 +9,13 @@ namespace Transmission.API.RPC.Test
     public class MethodsTest
     {
         const string FILE_PATH = "./Data/ubuntu-10.04.4-server-amd64.iso.torrent";
-        const string HOST = "";
+        const string HOST = "http://192.168.1.50:9091/transmission/rpc";
         const string SESSION_ID = "";
 
         Client client = new Client(HOST, SESSION_ID);
 
         [TestMethod]
-        public void TorrentAddAndRemove()
+        public void AddTorrent()
         {
             if (!File.Exists(FILE_PATH))
                 throw new Exception("Torrent file not found");
@@ -33,6 +33,19 @@ namespace Transmission.API.RPC.Test
             };
 
             var torrentInfo = client.AddTorrent(torrent);
+        }
+
+        [TestMethod]
+        public void GetTorrent()
+        {
+            var allTorrents = client.GetTorrents(TorrentFields.ALL_FIELDS);
+            var oneTorrent = client.GetTorrents(TorrentFields.ALL_FIELDS, new int[] { 42 });
+        }
+
+        [TestMethod]
+        public void RemoveTorrent()
+        {
+            client.RemoveTorrents(new int[] { 41 }); 
         }
     }
 }
